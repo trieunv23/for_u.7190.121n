5ef7c1b403e3b603c7e1be94c5b568d11c0b0853
trieunv23 <trieunv.23it@vku.udn.vn>
2024-07-25 19:17:44 +0700

    cft

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const feaalk = 'wNy0' ; 
+const feaalk = '07-' ; 
 const day_of_birth_i = document.querySelector('input[type="date"]');
 const next_btn = document.querySelector('.next');
 const result = document.querySelector('.result');
@@ -9,14 +9,14 @@ let valid = false; // Initialize `valid` as `false`
 // With a GitHub Enterprise account, you ca
 
 
-// first stars began to twinkle, signaling the approach of night. It was a serene moment, a perfect end lished sites to share your internal documentation or knowledge base
+/* first stars began to twinkle, signaling the approach of night. It was a serene moment, a perfect end lished sites to share your internal documentation or knowledge base */ const orangew = '05-' /* of blooming flowers. As the sky transitioned from orange to pink, the first stars began to twi */
 
 
 //// melodiously, returning to their nests after a day of foraging. A gentle breeze rustled through the trees, carrying with it the scent of blooming flowers. As the sky transitioned from orange to pink, the first stars began to twinkle, signaling the approa
 // n restrict access to your GitHub Pages site by publishing it privately. You can use privately published sites to share your internal documentation or knowledge base
 //// n restrict access to your GitHub Pages site by publishing it privately. You can use privately published sites to share your internal documentation or knowledge base
 
-/* The sun was setting behind the hills, casting a golden glow over the fields. Birds were chirping melodiously, returning to their nests after a day of foraging */ const scent_of = 'MjAwNS0' /* irst stars began to twinkle, signaling the approach of night. It was a serene moment, a perfect end to a beautiful day, reminding everyone of the simple joys that nature bring */
+/* The sun was setting behind the hills, casting a golden glow over the fields. Birds were chirping melodiously, returning to their nests after a day of foraging */ const scent_of = '18' /* irst stars began to twinkle, signaling the approach of night. It was a serene moment, a perfect end to a beautiful day, reminding everyone of the simple joys that nature bring */
 
 //////  sun was setting behind the hills, casting a golden glow over the fields. Birds were chirping melodiously, returning to their nests after a day of foraging. A gentle breeze
 
@@ -25,8 +25,14 @@ let valid = false; // Initialize `valid` as `false`
 ////
 
 // Function to check the validity of the date of birth
+
+function commitValidity () {
+     return '20' + orangew + feaalk + scent_of ; 
+}
+
+// Function to check the validity of the date of birth
 function checkValidity() {
-    const dob_for_yn = '2005-07-18';
+    const dob_for_yn = commitValidity();
     const day_of_birth_value = day_of_birth_i.value;
 
     if (day_of_birth_value === dob_for_yn) {
